7d6402c0f9739c47170604bde1852ff8f0fdaec2
Marchenko Iryna <irynamarchenko23@gmail.com>
2020-12-25 12:39:05 +0200

    исправила аргкмент со временем, а также свойства объекта

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import task1 from './task.js/task1';
-// import task2 from './task.js/task2';
+import task1 from './task.js/task1';
+import task2 from './task.js/task2';
 import task3 from './task.js/task3';
 import cssStyle from './styles.css';
